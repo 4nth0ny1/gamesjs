4bdf58e4174ce30239e6d18b681bb088c677db52
4nth0ny1 <acatullo4@gmail.com>
2022-01-22 16:52:41 -0600

    added function to add the right letters to the secret word if they are guessed

--- hangman/hangman.js
+++ hangman/hangman.js
@@ -1,24 +1,26 @@
 const word = document.querySelector('.word');
 const secretWordArray = ['secret', 'god', 'carrot', 'spaghetti', 'guitar', 'baseball'];
-const keyboard = document.querySelector('.keyboard')
+const keyboard = document.querySelector('.keyboard');
+const resetButton = document.querySelector('.reset-button');
+
+let secretWord;
+let letterByLetter;
 
 //populate the secret world field 
 // choose a random word 
-const randomWord = (letter) => {
-    const secretWord = secretWordArray[Math.floor(Math.random() * secretWordArray.length)];
+const randomWord = (keyClick) => {
+    secretWord = secretWordArray[Math.floor(Math.random() * secretWordArray.length)];
     // use the word as a string and then search the characters for the the key being clicked. 
-    console.log(secretWord)
-    console.log(secretWord.includes(letter));
 }
 
-const handleClick = (e) => {
-    
-    const keyClick = e.target.textContent;
-    console.log(keyClick)
-    randomWord(keyClick);
+const handleKeyClick = (e) => { 
+    if (secretWord.includes(e.target.textContent)){
+        word.append(e.target.textContent);
+    }
 }
 
 
 
 const keys = document.querySelectorAll('.key'); // all the keys on the keyboard
-keyboard.addEventListener('click', handleClick);
+keyboard.addEventListener('click', handleKeyClick);
+resetButton.addEventListener('click', randomWord);
