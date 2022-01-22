26f6ac8d969c7099fd27f7e91feebcab8dd6cbaa
4nth0ny1 <acatullo4@gmail.com>
2022-01-22 16:39:11 -0600

    add new handleclick function and changed randomword function to use event.target

--- hangman/hangman.js
+++ hangman/hangman.js
@@ -1,17 +1,24 @@
 const word = document.querySelector('.word');
 const secretWordArray = ['secret', 'god', 'carrot', 'spaghetti', 'guitar', 'baseball'];
+const keyboard = document.querySelector('.keyboard')
 
 //populate the secret world field 
 // choose a random word 
-const randomWord = () => {
+const randomWord = (letter) => {
     const secretWord = secretWordArray[Math.floor(Math.random() * secretWordArray.length)];
-    const letterArray = secretWord.split('');
-    console.log(letterArray);
-    for (var i = 0; i < letterArray.length; i++) {
-        const changedWord = letterArray[i];
-        const newWord = word.push(changedWord);
-        console.log(newWord);
-    };
+    // use the word as a string and then search the characters for the the key being clicked. 
+    console.log(secretWord)
+    console.log(secretWord.includes(letter));
 }
 
-randomWord();
+const handleClick = (e) => {
+    
+    const keyClick = e.target.textContent;
+    console.log(keyClick)
+    randomWord(keyClick);
+}
+
+
+
+const keys = document.querySelectorAll('.key'); // all the keys on the keyboard
+keyboard.addEventListener('click', handleClick);
