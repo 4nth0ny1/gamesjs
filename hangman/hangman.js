75d1f8f98120f78fafac2c0507f9267961ebe9b8
4nth0ny1 <acatullo4@gmail.com>
2022-01-22 15:58:40 -0600

    add new class for keys and altered the randomword function

--- hangman/hangman.js
+++ hangman/hangman.js
@@ -5,7 +5,13 @@ const secretWordArray = ['secret', 'god', 'carrot', 'spaghetti', 'guitar', 'base
 // choose a random word 
 const randomWord = () => {
     const secretWord = secretWordArray[Math.floor(Math.random() * secretWordArray.length)];
-    word.textContent = secretWord;
+    const letterArray = secretWord.split('');
+    console.log(letterArray);
+    for (var i = 0; i < letterArray.length; i++) {
+        const changedWord = letterArray[i];
+        const newWord = word.push(changedWord);
+        console.log(newWord);
+    };
 }
 
 randomWord();
