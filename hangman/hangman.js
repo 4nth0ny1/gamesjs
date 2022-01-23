3f086905110566e8c075d10e313b80c751ec632a
4nth0ny1 <acatullo4@gmail.com>
2022-01-22 19:23:15 -0600

    added miss counter

--- hangman/hangman.js
+++ hangman/hangman.js
@@ -5,6 +5,7 @@ const resetButton = document.querySelector('.reset-button');
 
 let secretWord;         // by declaring these variables we can use them in other functions
 let hiddenLetters;
+let miss = 0;
 
 const randomWord = (keyClick) => {
     secretWord = secretWordArray[Math.floor(Math.random() * secretWordArray.length)];
@@ -16,16 +17,13 @@ const randomWord = (keyClick) => {
         letter.append(secretWord[i]);
         // give the element a class which will hide the letter 
         letter.classList.add('hide');
-        // append the hidden letter to the DOM
+        // add the hidden letter to the DOM
         word.append(letter)
     }
     // create a variable that holds all the elements we created (which are the hidden letters)
     hiddenLetters = document.querySelectorAll('.hide');
 
 }
-
-// what i will need to do is use the hiddenLetters array to be able to change the class from .hide to .show and make each letter visible
-
 
 const handleKeyClick = (e) => { 
     // loop through the hiddenLetters
@@ -34,10 +32,18 @@ const handleKeyClick = (e) => {
         if (hiddenLetters[i].textContent === e.target.textContent){
             // if this is true then change the class of the letter from .hide to .show
             hiddenLetters[i].classList.add('show')
-        }
+        } 
     }
 }
 
-// const keys = document.querySelectorAll('.key'); // all the keys on the keyboard
+const missCounter = (e) => {
+    if (!secretWord.includes(e.target.textContent)) {
+        miss++;
+        console.log(miss)
+    }
+}
+
+
 keyboard.addEventListener('click', handleKeyClick);
+keyboard.addEventListener('click', missCounter);
 resetButton.addEventListener('click', randomWord);
