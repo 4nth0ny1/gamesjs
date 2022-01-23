1258f2249c660fc21dabb2950db307b36992a7a6
4nth0ny1 <acatullo4@gmail.com>
2022-01-23 16:25:52 -0600

    add reset button and removed bugs. resets the dom and resets the deck

--- video-poker/poker.js
+++ video-poker/poker.js
@@ -14,6 +14,7 @@ let array = [
     'Kc','Kd','Kh','Ks',
     'Ac','Ad','Ah','As',    
 ];
+
 let hand; 
 
 // fisher-yates shuffle ( o(n) : time complexity ) the array, then pop the last five elements.
@@ -60,6 +61,7 @@ const renderHand = () => {
         indyCardContainer.append(cardButton);
         cardContainer.append(indyCardContainer);
     }
+    console.log(`Deck after deal: ${array.length}`);
 }
 
 const dealAgain = () => {
@@ -68,6 +70,23 @@ const dealAgain = () => {
     for (let i = 0; i < childNodes.length; i++) {
         cardContainer.removeChild(childNodes[i]);
     }
+
+    array = [
+        '2c','2d','2h','2s',
+        '3c','3d','3h','3s',
+        '4c','4d','4h','4s',
+        '5c','5d','5h','5s',
+        '6c','6d','6h','6s',
+        '7c','7d','7h','7s',
+        '8c','8d','8h','8s',
+        '9c','9d','9h','9s',
+        '10c','10d','10h','10s',
+        'Jc','Jd','Jh','Js',
+        'Qc','Qd','Qh','Qs',
+        'Kc','Kd','Kh','Ks',
+        'Ac','Ad','Ah','As',    
+    ];
+    console.log(`Deck after reset: ${array.length}`);
 }
 
 const dealButton = document.querySelector('.button');
